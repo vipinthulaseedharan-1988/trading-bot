9e4e05be0f820d7987378dfb580393cbfe52fab5
Renata <vipin.thulaseedharan@gmail.com>
2026-07-14 01:46:09 +0400

    Untrack secrets, cache, data and logs

--- test_execution_quick.py
+++
@@ -1,7 +0,0 @@
-from src.bot.execution.alpaca_broker import submit_target_orders, get_equity
-
-print("Paper account equity: $", get_equity())
-
-# Use the same picks your strategy made
-weights = {"QQQ": 0.5, "IWM": 0.5}
-submit_target_orders(weights)
